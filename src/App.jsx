5116d4fa7ac9faa025d088eb35aadc505d827f53
Chang Wang Ngen <ngenchangwang@gmail.com>
2023-05-30 12:12:08 +0800

    Add Experience Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+function Experience() {
+	return (
+		<>
+			<h3>Software Developer</h3>
+			<h4>Sigma Lab Sdn Bhd</h4>
+		</>
+	);
+}
+
 function App() {
 	return (
 		<>
 			<h1>Chang Wang</h1>
 			<h2>React Full Stack Web Developer</h2>
+			<Experience />
 		</>
 	);
 }
